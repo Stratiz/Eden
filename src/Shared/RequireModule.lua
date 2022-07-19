b017acad17e78dacc571b9ab580a2efd45e92859
Stratiz <36082806+Stratiz@users.noreply.github.com>
2022-07-19 04:01:23 -0500

    change: Refactored require module to support luaU optimizations and auto initialization

--- src/Shared/RequireModule.lua
+++ src/Shared/RequireModule.lua
@@ -1,124 +1,238 @@
 -- @Stratiz 2021 :)
 
+local RequireModule = {}
 --- local ReplicatedStorage = game:GetService("ReplicatedStorage")
---- local require = require(ReplicatedStorage.SharedModules:WaitForChild("RequireModule"))
+--- local require = require(ReplicatedStorage:WaitForChild("SharedModules"):WaitForChild("RequireModule"))
+
+-- Constants
+local FIND_TIMEOUT = 3
+local LONG_LOAD_TIMEOUT = 4
+
 ---
 local RunService = game:GetService("RunService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
 -- Aggregate modules
 local Modules = {}
+local ModuleCount = 0
+local InitalizedModules = false
 
 local ModulePaths = {
-	RunService:IsClient() and Players.LocalPlayer:WaitForChild("PlayerScripts"):WaitForChild("ClientModules") or game:GetService("ServerScriptService"):WaitForChild("ServerModules"),
-	ReplicatedStorage:WaitForChild("SharedModules"),
+	-- Core paths
+	RunService:IsClient() and {
+		Alias = "Client",
+		Instance = Players.LocalPlayer:WaitForChild("PlayerScripts"):WaitForChild("ClientModules"),
+	} or {
+		Alias = "Server",
+		Instance =  game:GetService("ServerScriptService"):WaitForChild("ServerModules"),
+	},
+	{
+		Alias = "Shared",
+		Instance = ReplicatedStorage:WaitForChild("SharedModules"),
+	},
+	-- Custom paths
+	
 }
 
-local ModuleCount = 0
-local function AddModule(Module : Instance)
-	if Module:IsA("ModuleScript") then
-		if not Modules[Module.Name] then
-			Modules[Module.Name] = {
-				Instance = Module,
-				Name = Module.Name,
-				State = "INACTIVE",
-				DuplicateCount = 0,
-				RequiredBy = nil,
-			}
-			ModuleCount += 1
-		else
-			Modules[Module.Name].DuplicateCount += 1
-			warn(Modules[Module.Name].DuplicateCount + 1,"modules with the same name '",Module.Name,"' detected. Please resolve this to prevent unexpected behavior.")
-		end
+
+local function GetModulePathFromAlias(pathData : {}, module : ModuleScript)
+	local CurrentParent = module
+	local OrderedInstanceTable = {}
+	repeat
+		table.insert(OrderedInstanceTable, 1, CurrentParent.Name)
+		CurrentParent = CurrentParent.Parent
+	until CurrentParent == pathData.Instance or CurrentParent == game
+	return pathData.Alias.."/"..table.concat(OrderedInstanceTable, "/")
+end
+
+local function AddModule(pathData : {}, module : Instance)
+	if module:IsA("ModuleScript") then
+		local ModuleName = module.Name
+
+		local NewModuleData = {
+			Instance = module,
+			Name = ModuleName,
+			Path = GetModulePathFromAlias(pathData, module),
+			State = "INACTIVE",
+			RequiredBy = {},
+		}
+		ModuleCount += 1
+
+		table.insert(Modules, NewModuleData)
 	end
 end
 
-for _,ModuleFolder in ipairs(ModulePaths) do
-	for _,Module in pairs(ModuleFolder:GetDescendants()) do	
-		AddModule(Module)
+for _,PathData in ipairs(ModulePaths) do
+	for _,Module in pairs(PathData.Instance:GetDescendants()) do
+		AddModule(PathData,Module)
 	end
-	ModuleFolder.DescendantAdded:Connect(function(Module)
-		AddModule(Module)
+	PathData.Instance.DescendantAdded:Connect(function(Module)
+		if InitalizedModules == true then
+			warn("Module",Module.Name,"replicated late to",PathData.Alias,"module folder. This may cause unexpected behavior.")
+		end
+		AddModule(PathData,Module)
 	end)
 end
 
-----
-local TIMEOUT = 2
-local NewRequire = function(TargetModuleName)
+
+local function FindCycle(targetModuleObject, _cycleTable)
+	local CycleTable = _cycleTable or {}
+	table.insert(CycleTable, targetModuleObject)
+	for _, Requirer in targetModuleObject.RequiredBy do
+		local FindResult = table.find(CycleTable, Requirer)
+		if FindResult then
+			local CycleString = ""
+			for Index, ModuleData in ipairs(CycleTable) do
+				if Index >= FindResult then 
+					CycleString ..= ModuleData.Name .. " -> "
+				end
+			end
+			CycleString ..= CycleTable[FindResult].Name
+			return CycleString
+		else
+			return FindCycle(Requirer, CycleTable)
+		end
+	end
+	return nil
+end
+
+local function FindModule(targetString : string)
+	local Found = {}
+	for _,ModuleData in ipairs(Modules) do
+		if ModuleData.Name == targetString or ModuleData.Path == targetString then
+			table.insert(Found, ModuleData)
+		end
+	end
+	if #Found > 1 then
+		warn("Multiple modules found with the name '"..targetString.."'. To clarify, please use the path instead. (ex: Shared/Framework/MODULE)")
+	end
+	return Found[1]
+end
+
+local CurrentlyLoadingTree = {}
+local NewRequire = function(query : string | ModuleScript)
 	-- Default require functionality
-	if typeof(TargetModuleName) == "Instance" then
-		return require(TargetModuleName)
+	if typeof(query) == "Instance" then
+		return require(query)
 	end
 
 	local CurrentTimeout = 0
-	local TargetModuleData
-	local function FindModule()
-		for ModuleName,ModuleData in pairs(Modules) do
-			if ModuleData.Instance:IsA("ModuleScript") and ModuleName == TargetModuleName then
-				TargetModuleData = ModuleData
-			end
-		end
-	end
+	local TargetModuleData = FindModule(query)
+	
 	local CurrentModuleCount = 0
-	while not TargetModuleData and CurrentTimeout < TIMEOUT do
+	while not TargetModuleData and CurrentTimeout < FIND_TIMEOUT do
 		if CurrentModuleCount ~= ModuleCount then
 			CurrentModuleCount = ModuleCount
-			FindModule()
+			FindModule(query)
 		end
 		local Delta = task.wait()
 		CurrentTimeout += Delta
 	end
+
 	if not TargetModuleData then
-		error("Module "..TargetModuleName.." not found",3)
+		error("Module "..query.." not found",3)
 	else
 		if TargetModuleData.State == "INACTIVE" then
 			TargetModuleData.State = "LOADING"
+			if CurrentlyLoadingTree[1] then
+				table.insert(TargetModuleData.RequiredBy, CurrentlyLoadingTree[#CurrentlyLoadingTree])
+			end
+			table.insert(CurrentlyLoadingTree, TargetModuleData)
 		end
 		local ToReturn
 		task.spawn(function()
 			local TimeStart = tick()
-			while not ToReturn do
+			while TargetModuleData.State == "LOADING" do
 				task.wait()
-				if tick() - TimeStart > 3 then
-					warn("Module",TargetModuleName,"is taking a long time to load.")
+				if tick() - TimeStart > LONG_LOAD_TIMEOUT then
+					warn("Module",query,"is taking a long time to load.")
 					break
 				end
 			end
 		end)
-		local RequireScript = getfenv(2)["script"]
-		if RequireScript and RequireScript:IsA("ModuleScript") then
-			local InvokerModuleData = Modules[RequireScript.Name]
-			if InvokerModuleData and InvokerModuleData.Instance == RequireScript then
-				if TargetModuleData.State == "LOADING" then
-
-					-- Check for cyclicals
-					if InvokerModuleData.RequiredByModule then
-						local CyclicalPathString = InvokerModuleData.Name
-						local CurrentRequesterData = InvokerModuleData
-						while InvokerModuleData.RequiredByModule and InvokerModuleData.RequiredByModule.Instance ~= TargetModuleData.Instance do
-							if CurrentRequesterData.RequiredByModule then
-								CurrentRequesterData = CurrentRequesterData.RequiredByModule
-								CyclicalPathString = CurrentRequesterData.Name .." -> " .. CyclicalPathString
-							else
-								break
-							end
-						end
-						if CurrentRequesterData.Instance == TargetModuleData.Instance then
-							warn("Cyclical require detected: (",CyclicalPathString.." -> "..TargetModuleData.Name, ") \n\nPlease resolve this to prevent unexpected behavior.")
-						end
-					end
-				end
-				TargetModuleData.RequiredByModule = Modules[RequireScript.Name]
+		
+		-- Check for cyclic dependencies
+		if TargetModuleData.State == "LOADING" then
+			local CycleString = FindCycle(TargetModuleData)
+			if CycleString then
+				warn("Cyclical require detected: ("..CycleString..") \n\nPlease resolve this to prevent unexpected behavior.")
 			end
 		end
-		
+
 		ToReturn = require(TargetModuleData.Instance)
 		if TargetModuleData.State == "LOADING" then
 			TargetModuleData.State = "ACTIVE"
+			table.remove(CurrentlyLoadingTree, #CurrentlyLoadingTree)
 		end
 		return ToReturn
 	end
 end
 
+function RequireModule:InitModules(explictRequires : {}?)
+	if InitalizedModules == false then
+		InitalizedModules = true
+		explictRequires = explictRequires or {}
+		-- Get Init data
+		local LowestPriority = math.huge
+		for _,ModuleData in ipairs(Modules) do
+			local Success, RequiredData = pcall(function()
+				return NewRequire(ModuleData.Path)
+			end)
+			
+			if not Success then
+				warn("Module",ModuleData.Path,"failed to auto-load:",RequiredData)
+			end
+			if type(RequiredData) == "table" then
+				local TargetPriority = rawget(RequiredData,"_Priority") or math.huge
+				ModuleData._AutoInitData = {
+					Priority = TargetPriority,
+					Init = rawget(RequiredData,"Init"),
+					RequiredData = RequiredData
+				}
+				if TargetPriority < LowestPriority then
+					LowestPriority = TargetPriority
+				end
+			else
+				ModuleData._AutoInitData = {
+					Priority = math.huge,
+				}
+			end
+		end
+		LowestPriority = LowestPriority > 0 and 0 or LowestPriority
+		for _,ModuleData in ipairs(Modules) do
+			local TargetIndex = table.find(explictRequires, ModuleData.Path)
+			if TargetIndex then
+				ModuleData._AutoInitData.Priority = LowestPriority - ((#explictRequires + 1) - TargetIndex)
+			end
+		end
+
+		-- Sort Modules by priority
+		table.sort(Modules, function(a, b)
+			return a._AutoInitData.Priority < b._AutoInitData.Priority
+		end)
+		
+		print(explictRequires,Modules)
+		-- Auto initalize modules
+		for _,ModuleData in ipairs(Modules) do
+			if ModuleData._AutoInitData.Init then
+				ModuleData._AutoInitData.Init(ModuleData._AutoInitData.RequiredData)
+				ModuleData._AutoInitData = nil
+			end
+		end
+	else
+		error("You can only initalize modules once!")
+	end
+end
+
 _G.require = NewRequire
-return NewRequire
+shared.require = NewRequire
+
+local CallMetaTable = {
+	__call = function(_, ...)
+		return NewRequire(...)
+	end
+}
+setmetatable(shared,CallMetaTable)
+setmetatable(RequireModule,CallMetaTable)
+
+return RequireModule
